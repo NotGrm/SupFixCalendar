c8daaf2c74149beca27764039debfcc91e3ea36d
NotGrm <notgrm@gmail.com>
2012-11-15 11:52:57 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SupFixCalendar::Application.config.secret_token = '41248bc1b1bce3b67f9cd02343fd2beb23db23cdadd2d136fbb0a8c86d8c500e684eda5c642f983554031c43a15892a76c21c944594eed06804857008736c99d'
