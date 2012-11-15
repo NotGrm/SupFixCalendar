e3ec5dcb3e4e5fa98e3e5f2f8a01e4a6cf247e65
NotGrm <notgrm@gmail.com>
2012-11-15 17:31:42 +0100

    It's Works

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,35 @@
+#require 'ftools'
+require 'icalendar'
+
 class HomeController < ApplicationController
   def home
   end
 
-  def upload
+  def file_upload
+  	tmp = params[:ics].tempfile
+    
+    cals = Icalendar.parse(tmp)
+	cal = cals.first
+
+	converted = Icalendar::Calendar.new
+
+	# Now you can access the cal object in just the same way I created it
+	cal.events.each do |event|
+		event.dtstart.icalendar_tzid = ""
+		event.dtend.icalendar_tzid = ""
+
+		converted.add_event(event)
+	end
+
+	file = Tempfile.new(["fixed", ".ics"], 'tmp')
+	
+	begin
+		file.write(converted.to_ical)
+		send_file file.path
+	ensure
+		file.close
+		file.unlink   # deletes the temp file
+	end
+
   end
 end
