9fe2fbe4dd9bb9f706f3c71d79d653bbd00d5121
NotGrm <notgrm@gmail.com>
2012-11-18 17:15:55 +0100

    Add default locale for unsupported locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,13 @@ class ApplicationController < ActionController::Base
 
 	def set_locale
 		logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-		I18n.locale = extract_locale_from_accept_language_header || I18n.default_locale
+		supported_locales = ["en", "fr"]
+		extracted_locale = extract_locale_from_accept_language_header
+		I18n.locale = supported_locales.index(extracted_locale)? extracted_locale : I18n.default_locale
+		# I18n.locale = extract_locale_from_accept_language_header || I18n.default_locale
 		logger.debug "* Locale set to '#{I18n.locale}'"
 	end
-	
+
 	private
 	def extract_locale_from_accept_language_header
 		request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
